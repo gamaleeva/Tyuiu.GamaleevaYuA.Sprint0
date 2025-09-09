9544f406a9c0834c7d50814542ef30012ab1478c
Юлия Гамалеева <Tyuiu.GamaleevaYuA@outlook.com>
2025-09-09 17:29:39 +0300

    Task 2

---
+++ Tyuiu.GamaleevaYuA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GamaleevaYuA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GamaleevaYuA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Игорь", res);
+        }
+    }
+}
---
+++ Tyuiu.GamaleevaYuA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.GamaleevaYuA.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Игорь"));
+        Console.ReadKey();
+    }
+}
